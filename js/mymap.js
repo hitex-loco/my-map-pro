c68252d3085515db6fba2439927d5f8916786e6d
hitex-loco <hitesh.kumar@iic.ac.in>
2017-07-08 14:47:10 +0530

    More layers added

--- js/mymap.js
+++ js/mymap.js
@@ -1,7 +1,26 @@
 var mymap = L.map('mapid').setView([28.61, 77.2], 6);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+
+L.marker([28.61, 77.2]).addTo(mymap);
+
+function myFn() {
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 	maxZoom: 18,
 	id: 'mapbox.streets',
 	accessToken: 'pk.eyJ1IjoiaGl0ZXlhZGF2IiwiYSI6ImNqNHRmaWV0eTA3a3gzMnI0eXlqdTF3ejcifQ.EH7i5oO_JT81KYUPYKG_JQ'
-}).addTo(mymap)
+}).addTo(mymap);
+}
+
+function myFn1() {
+	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mymap);
+}
+
+function myFn2() {
+	L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(mymap);
+}
